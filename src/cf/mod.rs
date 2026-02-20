b707f3e9617521261b856c90ba136cd24532a31e
Vaivaswatha Nagaraj <vaivaswatha.nagaraj@fuel.sh>
2026-02-20 11:54:43 +0530

    Provide a ToCFDialect interface

--- src/cf/mod.rs
+++ src/cf/mod.rs
@@ -1,5 +1,24 @@
 //! Control-flow dialect for Pliron.
 
-pub mod attributes;
+use pliron::derive::op_interface;
+
+use pliron::irbuild::match_rewrite::MatchRewriter;
+use pliron::op::Op;
+use pliron::{context::Context, result::Result};
+
 pub mod ops;
 pub mod to_llvm;
+
+/// Interface for rewriting to CF dialect.
+#[op_interface]
+pub trait ToCFDialect {
+    /// Rewrite [self] to CF dialect.
+    fn rewrite(&self, ctx: &mut Context, rewriter: &mut MatchRewriter) -> Result<()>;
+
+    fn verify(_op: &dyn Op, _ctx: &Context) -> Result<()>
+    where
+        Self: Sized,
+    {
+        Ok(())
+    }
+}
